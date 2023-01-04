afd668bf359f40e4cc8a34c3f1035e427a8e6de8
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-04 09:08:22 +0800

    Initial commit

---
+++ 1 - Return Sum.js
@@ -0,0 +1,13 @@
+// Create a function sum() that returns the sum of the two numbers passed as its arguments.
+
+// For example, sum(2,5) should return 7; sum(3,10) should return 13.
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+
+sum(3,2)
+sum(3,10)
+sum(-3,4)
+sum(-5,10)
