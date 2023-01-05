efd4953956b325e8f516d4a7b3b393c548c7fc32
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-05 11:41:19 +0800

    Initial commit

---
+++ 3 - Fahrenheit to Celsius.js
@@ -0,0 +1,9 @@
+// Kelvin wants to convert between temperature scales. Create celciusToFahrenheit(cDegrees) that accepts a number of degrees in Celcius, and returns the equivalent temperature as expressed in Fahrenheit degrees. For review, Fahrenheit = (9/5 * Celsius) + 32.
+
+function celciusToFahrenheit(cDegrees) {
+  return (9/5 * cDegrees) + 32;
+}
+
+celciusToFahrenheit(0)
+celciusToFahrenheit(10)
+celciusToFahrenheit(100) 
